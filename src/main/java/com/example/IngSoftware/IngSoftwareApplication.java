b294c95b330711a7059922edeacb9cde1f7d2573
Alex SC <crepy27@gmail.com>
2024-12-01 22:46:20 -0500

    añadido de SwaggerUI y funciones

--- src/main/java/com/example/IngSoftware/IngSoftwareApplication.java
+++ src/main/java/com/example/IngSoftware/IngSoftwareApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IngSoftwareApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IngSoftwareApplication.class, args);
     }
