78b317723f4a68ae638e9bd531d92d51e8442686
Andres0918 <chablaandres@gmail.com>
2024-12-05 09:40:44 -0500

    Pruebas - EndPoints - Chabloide

--- src/main/java/com/example/IngSoftware/controller/AlmacenController.java
+++ src/main/java/com/example/IngSoftware/controller/AlmacenController.java
@@ -2,7 +2,9 @@ package com.example.IngSoftware.controller;
 
 import java.util.List;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +27,16 @@ public class AlmacenController {
 
     // Endpoint para buscar un almacén por ID
     @GetMapping("/{id}")
-    public ResponseEntity<Almacen> buscarPorId(@PathVariable Long id) {
+    public ResponseEntity<?> obtenerAlmacenPorId(@PathVariable("id") Long id) {
         Almacen almacen = almacenService.buscarPorId(id);
-        return ResponseEntity.ok(almacen);
+        if (almacen != null) {
+            return ResponseEntity.ok(almacen);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("El almacén con ID " + id + " no fue encontrado");
+        }
     }
+
 
     // Endpoint para crear un nuevo almacén
     @PostMapping
@@ -39,10 +47,19 @@ public class AlmacenController {
 
     // Endpoint para actualizar un almacén existente
     @PutMapping("/{id}")
-    public ResponseEntity<Almacen> actualizarAlmacen(@PathVariable Long id, @RequestBody Almacen almacen) {
-        Almacen almacenActualizado = almacenService.actualizarAlmacen(id, almacen);
-        return ResponseEntity.ok(almacenActualizado);
+    public ResponseEntity<?> actualizarAlmacen(@PathVariable Long id, @RequestBody Almacen almacen) {
+        try {
+            Almacen almacenActualizado = almacenService.actualizarAlmacen(id, almacen);
+            return ResponseEntity.ok(almacenActualizado);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("El almacén con ID " + id + " no fue encontrado");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Ocurrió un error al intentar actualizar el almacén: " + e.getMessage());
+        }
     }
+
 
     // Endpoint para eliminar un almacén por ID
     @DeleteMapping("/{id}")
